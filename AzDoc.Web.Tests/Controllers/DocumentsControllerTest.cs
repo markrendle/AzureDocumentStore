2ded2aa8aafcc33be123fd275575846112a1be6a
Mark Rendle <mark@markrendle.net>
2010-10-05 08:39:22 -0700

    Refactored DocumentsController test

---
+++ AzDoc.Web.Tests/Controllers/DocumentsControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Routing;
+using AzDoc.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace AzDoc.Web.Tests.Controllers
+{
+    
+    
+    /// <summary>
+    ///This is a test class for DocumentsControllerTest and is intended
+    ///to contain all DocumentsControllerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class DocumentsControllerTest
+    {
+        private static DocumentsController CreateController(string data)
+        {
+            var controller = new DocumentsController();
+            controller.ControllerContext = new ControllerContext
+            {
+                Controller = controller,
+                RequestContext =
+                    new RequestContext(new MockHttpContextWithInputStream(data), new RouteData())
+            };
+            return controller;
+        }
+
+        /// <summary>
+        ///A test for Post
+        ///</summary>
+        // TODO: Ensure that the UrlToTest attribute specifies a URL to an ASP.NET page (for example,
+        // http://.../Default.aspx). This is necessary for the unit test to be executed on the web server,
+        // whether you are testing a page, web service, or a WCF service.
+        [TestMethod()]
+        public void PostTest()
+        {
+            // Arrange
+            var target =
+                CreateController(
+                    @"{""LastName"":""Smith"", ""Addresses"":[{""Postcode"":""P1""},{""Postcode"":""P2""}]}");
+            const string collection = "posts";
+
+            // Act
+            var actual = target.Post(collection) as ContentResult;
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.Content.Lines().Contains("Collection: posts"));
+            Assert.IsTrue(actual.Content.Lines().Contains("Smith#P1"));
+            Assert.IsTrue(actual.Content.Lines().Contains("Smith#P2"));
+        }
+    }
+
+    static class StringEx
+    {
+        public static IEnumerable<string> Lines(this string str)
+        {
+            return str.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries).AsEnumerable();
+        }
+    }
+}
---
+++ AzDoc.Web.Tests/MockHttpContextWithInputStream.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Text;
+using System.Web;
+
+namespace AzDoc.Web.Tests
+{
+    public class MockHttpContextWithInputStream : HttpContextBase
+    {
+        private readonly HttpRequestBase _request;
+        public MockHttpContextWithInputStream(string postData)
+        {
+            _request = new MockHttpRequestWithInputStream(postData);
+        }
+
+        public override HttpRequestBase Request
+        {
+            get
+            {
+                return _request;
+            }
+        }
+
+        private class MockHttpRequestWithInputStream : HttpRequestBase
+        {
+            public MockHttpRequestWithInputStream(string inputStreamData)
+            {
+                _inputStream = new MemoryStream(Encoding.UTF8.GetBytes(inputStreamData));
+            }
+            private readonly MemoryStream _inputStream;
+            public override Stream InputStream
+            {
+                get
+                {
+                    return _inputStream;
+                }
+            }
+        }
+    }
+}
